4fa0ef06db4c384f13b97794d3d98849ea151f78
linothomas14 <linolowlevel@gmail.com>
2022-05-24 17:41:13 +0700

    migrate

--- app.py
+++ app.py
@@ -1,124 +1,183 @@
 import tkinter as tk
-from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+import play
+LARGE_FONT= ("Verdana", 12)
+con = False
 
-import utils as U
+class SeaofBTCapp(tk.Tk):
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        ## Setting up Initial Things
-        self.title("Sample Tkinter Structuring")
-        self.geometry("720x550")
-        self.resizable(True, True)
-        self.iconphoto(False, tk.PhotoImage(file="assets/title_icon.png"))
-    
-        ## Creating a container
-        container = tk.Frame(self, bg="#8AA7A9")
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        self.geometry("1000x700")
         container.pack(side="top", fill="both", expand = True)
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-
-        ## Initialize Frames
         self.frames = {}
-        self.HomePage = HomePage
-        self.Validation = Validation
 
-        ## Defining Frames and Packing it
-        for F in {HomePage, Validation}:
-            frame = F(self, container)
+        for F in (StartPage, BasicMode, LearnMaterial):
+
+            frame = F(container, self)
+
             self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")    
-           
-        self.show_frame(HomePage)
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
 
     def show_frame(self, cont):
+
         frame = self.frames[cont]
-        menubar = frame.create_menubar(self)
-        self.configure(menu=menubar)
-        frame.tkraise()                         ## This line will put the frame on front
- 
+        frame.tkraise()
+    
+    def play(self):
+        self.destroy()
+        play.main()
+    
+
+
+        
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = tk.Button(self, text="Play",
+                            command=lambda : self.activatedCam(controller))
+        button.pack()
+
+        button2 = tk.Button(self, text="Learn Material",
+                            command=lambda: controller.show_frame(LearnMaterial))
+        button2.pack()
+
+    def activatedCam(self,controller):
+        print("masuk func")
+        global con
+        con = True
+        print("con sebelum : ", con)
+        controller.show_frame(BasicMode)
+    # def play(self):
+    #     super.destroy()
+    #     import play
+
+
+class BasicMode(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.imgLabel = tk.Label(self)
+        self.imgLabel.pack(fill="both", expand="yes")
+
+        self.mainMenuButton = tk.Button(self,text="Play",
+                                command=self.mainMenu)
+        self.mainMenuButton.pack()
+        
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(LearnMaterial))
+        button2.pack()
+
+
+        imgLabel = tk.Label()
+        global con
+        while con:
+            print("pass")
+            # self.main()
+
+    def mainMenu(self):
+        self.destroy()
+        import app
+
+    def main(self):
+        pTime = 0
+        wCam, hCam = 800, 380
+        cap = cv2.VideoCapture(0)
+        cap.set(3, wCam)
+        cap.set(4, hCam)
+        global con
+        print(con)
+        while con :
+            print("helo")
+            success, img = cap.read()
+            img = cv2.flip(img, 1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = ImageTk.PhotoImage(image = Image.fromarray(img))
+            
+            self.imgLabel['image'] = img
+            self.update()
+
+        
 
 
 
-#---------------------------------------- HOME PAGE FRAME / CONTAINER ------------------------------------------------------------------------
+class LearnMaterial(tk.Frame):
 
-class HomePage(tk.Frame):
-    def __init__(self, parent, container):
-        super().__init__(container)
+    #Importing Photos
+    dir = "assets/"
+    image_list=[]
+    text_list = []
+    for i in range(5):
+        img = dir + str(i+1) + '.png'
+        text_list.append(str(i+1))    
+        image_list.append(img)
 
-        label = tk.Label(self, text="Home Page", font=('Times', '20'))
-        label.pack(pady=0,padx=0)
-
-        ## ADD CODE HERE TO DESIGN THIS PAGE
-
-    def create_menubar(self, parent):
-        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
-
-        ## Filemenu
-        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
-        menubar.add_cascade(label="File", menu=filemenu)
-        filemenu.add_command(label="New Project", command=lambda: parent.show_frame(parent.Validation))
-        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=parent.quit)  
-
-        ## proccessing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
-
-        ## help menu
-        help_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=U.about)
-        help_menu.add_separator()
-
-        return menubar
+    current = 0
+        
+        
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
 
-#---------------------------------------- Validation PAGE FRAME / CONTAINER ------------------------------------------------------------------------
+        label = tk.Label(self, text="Learn Material", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
 
-class Validation(tk.Frame):
-    def __init__(self, parent, container):
-        super().__init__(container)
+        self.imgWrapper = tk.Label(self, compound=tk.TOP)
+        self.imgWrapper.pack()
+        tk.Button(self, text='Previous picture', command=lambda: self.move(-1)).pack(side=tk.BOTTOM)
+        tk.Button(self, text='Next picture', command=lambda: self.move(+1)).pack(side=tk.BOTTOM)
+        button1 = tk.Button(self, text="Back to Main menu",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+        self.move(0)
+        print('test')
+    
+    def move(self,Rd):
 
-        label = tk.Label(self, text="Validation Page", font=('Times', '20'))
-        label.pack(pady=0,padx=0)
-
-        ## ADD CODE HERE TO DESIGN THIS PAGE
-
-    def create_menubar(self, parent):
-        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
-
-        ## Filemenu
-        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
-        menubar.add_cascade(label="File", menu=filemenu)
-        filemenu.add_command(label="New Project", command=lambda: parent.show_frame(parent.Validation))
-        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=parent.quit)  
-
-        ## proccessing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
-
-        ## help menu
-        help_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=U.about)
-        help_menu.add_separator()
-
-        return menubar
+        if not (0 <= self.current + Rd < len(self.image_list)):
+            return
+        self.current += Rd
+        image = Image.open(self.image_list[self.current])
+        photo = ImageTk.PhotoImage(image)
+        self.imgWrapper['text'] = "Number " + self.text_list[self.current]
+        self.imgWrapper['image'] = photo
+        self.imgWrapper.photo = photo
 
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
 
-    ## IF you find this useful >> Claps on Medium >> Stars on Github >> Subscription on youtube will help me
 
+            
+
+
+app = SeaofBTCapp()
+
+# cap = cv2.VideoCapture(0)
+
+app.mainloop()
+# while True :
+    # print('halo')
+    # success, img = cap.read()
+    # img = cv2.flip(img, 1)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = ImageTk.PhotoImage(Image.fromarray(img))
+    # app.frames["BasicMode"].imgLabel['image'] = img
+# app.update()
+    
